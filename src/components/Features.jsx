fbd0ef8ee7d251930e1fb93f9919190f41077aea
Francisco Rubial <franjorub@gmail.com>
2019-02-01 00:03:00 -0400

    added Features.jsx

---
+++ src/components/Features.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="container">
+      <div className="row">
+        <div className="col-12 col-sm-6">
+          <h3>Access your files, anywhere</h3>
+          <p>
+            the ability to use a smartphone, tablet, or computer to access your
+            account means your files follow you everywhere.
+          </p>
+        </div>
+        <div className="col-12 col-sm-6">
+          <h3>security you can trust</h3>
+          <p>
+            2-factor authentication and user-controlled encryption are just a
+            couple of the security features we allow to help secure your files.
+          </p>
+        </div>
+        <div className="col-12 col-sm-6">
+          <h3>Real-time collaboration</h3>
+          <p>
+            Securely share files and folders with friends, family and collegues
+            for live collaboration. No email attachments required.
+          </p>
+        </div>
+        <div className="col-12 col-sm-6">
+          <h3>Store any type of file</h3>
+          <p>
+            Whether you're sharing holidays photos or work documents, Fylo has
+            you covered allowing for all file types to be securely sotred and
+            shared.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
