f4e7b747c022d6ddf6ad43e97462cc84317e0e44
Francisco Rubial <franjorub@gmail.com>
2019-02-03 16:15:10 -0400

    Updates

---
+++ precache-manifest.c872063f3c85ca58975e43ebb9adb342.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/fylo/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "0e0e73416227634e8bfb",
+    "url": "/fylo/static/css/main.415b6dbf.chunk.css"
+  },
+  {
+    "revision": "63572bc12405c5fe3eae",
+    "url": "/fylo/static/js/1.63572bc1.chunk.js"
+  },
+  {
+    "revision": "bcaf398461dece0e2d4b",
+    "url": "/fylo/static/js/runtime~main.bcaf3984.js"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/fylo/static/media/logo.c71bf7bf.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/fylo/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/fylo/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "0e0e73416227634e8bfb",
+    "url": "/fylo/static/js/main.0e0e7341.chunk.js"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/fylo/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/fylo/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/fylo/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/fylo/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/fylo/static/media/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "048362ecde62e20ea294b12c1009efd8",
+    "url": "/fylo/index.html"
+  }
+];
